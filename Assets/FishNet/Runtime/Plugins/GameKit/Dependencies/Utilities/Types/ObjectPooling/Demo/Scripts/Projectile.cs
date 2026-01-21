cc693ff959c7e58dda6099c44a2fb95896b05978
Vyacheslav Denisov <vyach.denisov@gmail.com>
2026-01-21 10:01:35 +0200

    Fix deprecated warning

--- Assets/FishNet/Runtime/Plugins/GameKit/Dependencies/Utilities/Types/ObjectPooling/Demo/Scripts/Projectile.cs
+++ Assets/FishNet/Runtime/Plugins/GameKit/Dependencies/Utilities/Types/ObjectPooling/Demo/Scripts/Projectile.cs
@@ -32,7 +32,7 @@ namespace GameKit.Dependencies.Utilities.ObjectPooling.Examples
             // Used as our pretend overhead.
             for (int i = 0; i < 30; i++)
             {
-                _spawner = FindObjectOfType<ProjectileSpawner>();
+                _spawner = FindAnyObjectByType<ProjectileSpawner>();
                 _renderers = GetComponentsInChildren<MeshRenderer>();
             }
         }
