18acc7ad3fd7b2d0c9a9a597b3e7b21f466a20ed
Vyacheslav Denisov <vyach.denisov@gmail.com>
2026-01-25 17:58:53 +0200

    Add offline scene and host/port ui

---
+++ Assets/Scripts/Network/NetworkHudCanvases.cs
@@ -0,0 +1,176 @@
+﻿using FishNet.Managing;
+using FishNet.Transporting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Network
+{
+    public class NetworkHudCanvases : MonoBehaviour
+    {
+        #region Types.
+        /// <summary>
+        /// Ways the HUD will automatically start a connection.
+        /// </summary>
+        private enum AutoStartType
+        {
+            Disabled,
+            Host,
+            Server,
+            Client
+        }
+        #endregion
+
+        #region Serialized.
+        /// <summary>
+        /// What connections to automatically start on play.
+        /// </summary>
+        [Tooltip("What connections to automatically start on play.")]
+        [SerializeField]
+        private AutoStartType _autoStartType = AutoStartType.Disabled;
+        /// <summary>
+        /// Color when socket is stopped.
+        /// </summary>
+        [Tooltip("Color when socket is stopped.")]
+        [SerializeField]
+        private Color _stoppedColor;
+        /// <summary>
+        /// Color when socket is changing.
+        /// </summary>
+        [Tooltip("Color when socket is changing.")]
+        [SerializeField]
+        private Color _changingColor;
+        /// <summary>
+        /// Color when socket is started.
+        /// </summary>
+        [Tooltip("Color when socket is started.")]
+        [SerializeField]
+        private Color _startedColor;
+        [Header("Indicators")]
+        /// <summary>
+        /// Indicator for server state.
+        /// </summary>
+        [Tooltip("Indicator for server state.")]
+        [SerializeField]
+        private Image _serverIndicator;
+        /// <summary>
+        /// Indicator for client state.
+        /// </summary>
+        [Tooltip("Indicator for client state.")]
+        [SerializeField]
+        private Image _clientIndicator;
+
+        [SerializeField] private Transform inputParent;
+        [SerializeField] private TMPro.TMP_InputField hostnameInput;
+        [SerializeField] private TMPro.TMP_InputField portInput;
+        #endregion
+
+        #region Private.
+        /// <summary>
+        /// Found NetworkManager.
+        /// </summary>
+        private NetworkManager _networkManager;
+        /// <summary>
+        /// Current state of client socket.
+        /// </summary>
+        private LocalConnectionState _clientState = LocalConnectionState.Stopped;
+        /// <summary>
+        /// Current state of server socket.
+        /// </summary>
+        private LocalConnectionState _serverState = LocalConnectionState.Stopped;
+#if !ENABLE_INPUT_SYSTEM
+        /// <summary>
+        /// EventSystem for the project.
+        /// </summary>
+        private EventSystem _eventSystem;
+#endif
+        #endregion
+
+        private void Start()
+        {
+            _networkManager = FindObjectOfType<NetworkManager>();
+            if (_networkManager == null)
+            {
+                Debug.LogError("NetworkManager not found, HUD will not function.");
+                return;
+            }
+            else
+            {
+                UpdateColor(LocalConnectionState.Stopped, ref _serverIndicator);
+                UpdateColor(LocalConnectionState.Stopped, ref _clientIndicator);
+                _networkManager.ServerManager.OnServerConnectionState += ServerManager_OnServerConnectionState;
+                _networkManager.ClientManager.OnClientConnectionState += ClientManager_OnClientConnectionState;
+            }
+
+            if (_autoStartType == AutoStartType.Host || _autoStartType == AutoStartType.Server)
+                OnClick_Server();
+            if (!Application.isBatchMode && (_autoStartType == AutoStartType.Host || _autoStartType == AutoStartType.Client))
+                OnClick_Client();
+        }
+
+        private void OnDestroy()
+        {
+            if (_networkManager == null)
+                return;
+
+            _networkManager.ServerManager.OnServerConnectionState -= ServerManager_OnServerConnectionState;
+            _networkManager.ClientManager.OnClientConnectionState -= ClientManager_OnClientConnectionState;
+        }
+
+        /// <summary>
+        /// Updates img color baased on state.
+        /// </summary>
+        /// <param name = "state"></param>
+        /// <param name = "img"></param>
+        private void UpdateColor(LocalConnectionState state, ref Image img)
+        {
+            Color c;
+            if (state == LocalConnectionState.Started)
+                c = _startedColor;
+            else if (state == LocalConnectionState.Stopped)
+                c = _stoppedColor;
+            else
+                c = _changingColor;
+
+            img.color = c;
+        }
+
+        private void ClientManager_OnClientConnectionState(ClientConnectionStateArgs obj)
+        {
+            _clientState = obj.ConnectionState;
+            UpdateColor(obj.ConnectionState, ref _clientIndicator);
+            inputParent.gameObject.SetActive(_networkManager.IsOffline);
+        }
+
+        private void ServerManager_OnServerConnectionState(ServerConnectionStateArgs obj)
+        {
+            _serverState = obj.ConnectionState;
+            UpdateColor(obj.ConnectionState, ref _serverIndicator);
+            inputParent.gameObject.SetActive(_networkManager.IsOffline);
+        }
+
+        public void OnClick_Server()
+        {
+            if (_networkManager == null)
+                return;
+
+            _networkManager.TransportManager.Transport.SetPort(ushort.Parse(portInput.text));
+            if (_serverState != LocalConnectionState.Stopped)
+                _networkManager.ServerManager.StopConnection(true);
+            else
+                _networkManager.ServerManager.StartConnection();
+        }
+
+        public void OnClick_Client()
+        {
+            if (_networkManager == null)
+                return;
+
+            _networkManager.TransportManager.Transport.SetClientAddress(hostnameInput.text);
+            _networkManager.TransportManager.Transport.SetPort(ushort.Parse(portInput.text));
+            if (_clientState != LocalConnectionState.Stopped)
+                _networkManager.ClientManager.StopConnection();
+            else
+                _networkManager.ClientManager.StartConnection();
+        }
+    }
+}
