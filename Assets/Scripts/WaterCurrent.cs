51004e8b1a923f5dfa9c6ac19c925138b6c0642e
heikki-heinonen <heikki.heinonen@edu.turkuamk.fi>
2026-01-28 13:23:09 +0200

    WaterCurrent inspector rotating
    - rotating object in play mode through the component fields updates forward direction

--- Assets/Scripts/WaterCurrent.cs
+++ Assets/Scripts/WaterCurrent.cs
@@ -11,6 +11,7 @@ public class WaterCurrent : MonoBehaviour
 
     [SerializeField] private float length = 1.0f; // Z
     [SerializeField] private float width = 1.0f; // X
+    [SerializeField] private float rotationY;
 
     private void Awake()
     {
@@ -19,7 +20,7 @@ public class WaterCurrent : MonoBehaviour
 
     private void OnValidate()
     {
-        UpdateScale();
+        UpdateTransform();
     }
 
     private void FixedUpdate()
@@ -59,12 +60,18 @@ public class WaterCurrent : MonoBehaviour
         }
     }
 
-    // Updates object scale according to changes in inspector
-    void UpdateScale()
+    // Updates object scale and forward direction according to changes in inspector
+    void UpdateTransform()
     {
         Vector3 scale = transform.localScale;
         scale.z = length;
         scale.x = width;
         transform.localScale = scale;
+
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y = rotationY;
+        transform.eulerAngles = rotation;
+
+        direction = transform.forward;
     }
 }
