df3331a5ddd4a8c28890841403813b30431e71a4
Vyacheslav Denisov <vyach.denisov@gmail.com>
2026-02-04 23:24:02 +0200

    Removed unused stuff

--- Assets/Scripts/Network/LoadingScreenSceneProcessor.cs
+++ Assets/Scripts/Network/LoadingScreenSceneProcessor.cs
@@ -1,14 +1,10 @@
-using System;
 using UI;
 using FishNet.Managing.Scened;
-using UnityEngine;
 
 namespace Network
 {
     public class LoadingScreenSceneProcessor : DefaultSceneProcessor
     {
-        [SerializeField] private float minLoadingTime = 1f;
-        
         public override void LoadStart(LoadQueueData queueData)
         {
             base.LoadStart(queueData);
