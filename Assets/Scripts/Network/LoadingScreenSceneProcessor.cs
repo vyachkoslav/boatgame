cd50ba7b033e0cf2f71c403bee267ce3aa507218
Vyacheslav Denisov <vyach.denisov@gmail.com>
2026-02-04 22:24:27 +0200

    Smooth transition to loading

--- Assets/Scripts/Network/LoadingScreenSceneProcessor.cs
+++ Assets/Scripts/Network/LoadingScreenSceneProcessor.cs
@@ -8,32 +8,17 @@ namespace Network
     public class LoadingScreenSceneProcessor : DefaultSceneProcessor
     {
         [SerializeField] private float minLoadingTime = 1f;
-        private float timeToDisable;
-        private bool ended;
         
         public override void LoadStart(LoadQueueData queueData)
         {
             base.LoadStart(queueData);
             LoadingScreen.ShowLoadingScreen();
-            timeToDisable = minLoadingTime;
         }
 
         public override void LoadEnd(LoadQueueData queueData)
         {
             base.LoadEnd(queueData);
-            ended = timeToDisable > 0;
-            if (timeToDisable <= 0)
-                LoadingScreen.HideLoadingScreen();
-        }
-
-        private void Update()
-        {
-            timeToDisable -= Time.deltaTime;
-            if (timeToDisable <= 0 && ended)
-            {
-                LoadingScreen.HideLoadingScreen();
-                ended = false;
-            }
+            LoadingScreen.HideLoadingScreen();
         }
     }
 }
--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -1,10 +1,17 @@
 using FishNet;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class LoadingScreen : MonoBehaviour
     {
+        [SerializeField] private float timeToFullOpacity = 0.5f;
+        [SerializeField] private Graphic image;
+        [SerializeField] private GameObject enabledAfterFullOpacity;
+        
+        private float currentTime;
+        
         private void Start()
         {
             if (InstanceFinder.NetworkManager.HasInstance<LoadingScreen>())
@@ -16,6 +23,25 @@ namespace UI
             InstanceFinder.NetworkManager.RegisterInstance(this);
             DontDestroyOnLoad(gameObject);
             HideLoadingScreen();
+        }
+
+        private void OnEnable()
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+            enabledAfterFullOpacity?.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (currentTime > timeToFullOpacity)
+            {
+                enabledAfterFullOpacity?.SetActive(true);
+                return;
+            }
+            
+            currentTime += Time.deltaTime;
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 
+                Mathf.Clamp01(currentTime/timeToFullOpacity));
         }
 
         public static void ShowLoadingScreen()
