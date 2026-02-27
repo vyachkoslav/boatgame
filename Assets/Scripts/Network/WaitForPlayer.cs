aa19fcf167946015c5d4db96f7acf8f870cb0ac0
Vyacheslav Denisov <vyach.denisov@gmail.com>
2026-02-27 05:15:20 +0200

    Add wait for other player skip by pressing space

--- Assets/Scripts/Network/WaitForPlayer.cs
+++ Assets/Scripts/Network/WaitForPlayer.cs
@@ -3,13 +3,16 @@ using FishNet.Connection;
 using FishNet.Object;
 using UI;
 using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 namespace Network
 {
     public class WaitForPlayer : NetworkBehaviour
     {
         private static WaitForPlayer instance;
-        public static bool IsWaiting => !PlayerManager.Instance.BothPlayersAssigned;
+        public static bool IsWaiting => !PlayerManager.Instance.BothPlayersAssigned && !instance.spacePressed;
+        
+        private bool spacePressed;
 
         private void Awake()
         {
@@ -17,8 +20,20 @@ namespace Network
             instance = this;
         }
         
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+        private void Update()
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame && !spacePressed)
+            {
+                spacePressed = true;
+                WaitForPlayerUI.Instance.StopWait();
+            }
+        }
+#endif
+        
         public override void OnStartClient()
         {
+            spacePressed = false;
             PlayerManager.OnLeftPlayerAssigned(OnPlayerAssigned);
             PlayerManager.OnRightPlayerAssigned(OnPlayerAssigned);
         }
