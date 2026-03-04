61f69ef5d3de7d275b858adc600f2b59b9cb9176
ElDiamondo <utile.peach2@gmail.com>
2026-03-04 17:40:48 +0200

    Buttons In Main Menu Play A Sound Now

--- Assets/Scripts/Audio/OfflineButtonClickSound.cs
+++ Assets/Scripts/Audio/OfflineButtonClickSound.cs
@@ -1,18 +1,8 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OfflineButtonClickSound : MonoBehaviour
 {
-    void Start()
-    {
-        Button[] buttons = FindObjectsByType<Button>(FindObjectsSortMode.None);
-        foreach (Button button in buttons)
-        {
-            button.onClick.AddListener(ButtonSound);
-        }
-    }
-
     public void ButtonSound()
     {
         Debug.Log("Button clicked! Playing sound.");
